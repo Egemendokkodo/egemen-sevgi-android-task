deacc51ebecb449b18491f7757b78f69948fac91
Egemen Sevgi <egemen2001sevgi@hotmail.com>
2023-03-05 01:21:21 +0300

    Revert "retrieved the data from the api and implemented it into the recyclerview"
    This reverts commit af52eb8d34d0523977061a385bb8b3d3dee00fd3.

--- app/src/main/java/com/uygulamalarim/androidtaskegemensevgi/DataModel/ModelClass.kt
+++
@@ -1,8 +0,0 @@
-package com.uygulamalarim.androidtaskegemensevgi.DataModel
-
-import com.google.gson.annotations.SerializedName
-
-data class ModelClass(
-    @SerializedName("items")
-    val items: List<ModelClassItem>
-)
